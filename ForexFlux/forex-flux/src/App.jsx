2987140cafe20135a1c107c10652bd08690a125c
SlytherinKid <mr.shubhamkr2000@gmail.com>
2025-04-14 20:46:38 +0530

    updated forex-flux styling

--- ForexFlux/forex-flux/src/App.jsx
+++ ForexFlux/forex-flux/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import { InputBox } from './components'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
 
@@ -25,35 +25,34 @@ const App = () => {
 
   return (
     <div
-            className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
-            style={{
-                backgroundImage: `url('https://images.unsplash.com/photo-1580519542036-c47de6196ba5?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
-            }}
+            className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-gradient-to-r from-orange-400 via-red-500 to-pink-500 bg-no-repeat"
+            
         >
             <div className="w-full">
-                <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                <div className="w-full max-w-md mx-auto shadow-2xl rounded-lg p-5 backdrop-blur-sm bg-white/30">
                     <form
                         onSubmit={(e) => {
                             e.preventDefault();
                             convert()
                         }}
                     >
-                        <div className="w-full mb-1">
+                        <div className="w-full mb-1 ">
                             <InputBox
                                 label="From"
                                 amount={amount}
                                 currencyOptions={options}
                                 onCurrencyChange={(currency) => setAmount(amount)}
                                 selectCurrency={from}
+                                onAmountChange={(amount) => setAmount(amount)}
                             />
                         </div>
                         <div className="relative w-full h-0.5">
                             <button
                                 type="button"
-                                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-sm shadow-2xl before:ease relative overflow-hidden border border-green-500 bg-green-500 text-white shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-green-500 hover:before:-translate-x-40 px-2 py-0.5"
                                 onClick={swap}
                             >
-                                swap
+                                Swap
                             </button>
                         </div>
                         <div className="w-full mt-1 mb-4">
@@ -62,12 +61,12 @@ const App = () => {
                                 amount={convertedAmount}
                                 currencyOptions={options}
                                 onCurrencyChange={(currency) => setTo(currency)}
-                                selectCurrency={from}
+                                selectCurrency={to}
                                 amountDisable
                             />
                         </div>
-                        <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
-                            Convert {from.toUpperCase()} to {to.toUpperCase()}
+                        <button type="submit" className="w-full rounded-sm text-red hover:before:bg-redborder-red-500 relative h-[50px] w-40 overflow-hidden border border-blue-500 bg-white px-3 text-blue-500 shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-blue-500 before:transition-all before:duration-500 hover:text-white hover:shadow-blue-500 hover:before:left-0 hover:before:w-full">
+                            <span className='relative z-10'>Convert {from.toUpperCase()} to {to.toUpperCase()}</span>
                         </button>
                     </form>
                 </div>
