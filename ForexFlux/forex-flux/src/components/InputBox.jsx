3055518e91d520cb5dff9c9d5d07aa27ad12d15a
SlytherinKid <mr.shubhamkr2000@gmail.com>
2025-04-15 21:43:35 +0530

    updated forex-flux styling

--- ForexFlux/forex-flux/src/components/InputBox.jsx
+++ ForexFlux/forex-flux/src/components/InputBox.jsx
@@ -8,7 +8,7 @@ const InputBox = ({label, amount, onAmountChange, onCurrencyChange,
     return (
         <div className={`bg-white p-3 shadow-2xl rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId} className="text-indigo-950/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
