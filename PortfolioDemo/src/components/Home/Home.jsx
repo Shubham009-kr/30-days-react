c812512b83f009b9d08dda2076b353d1e7e6d4ec
SlytherinKid <mr.shubhamkr2000@gmail.com>
2025-04-16 23:47:54 +0530

    added home and about pages on  PortfolioDemo

--- PortfolioDemo/src/components/Home/Home.jsx
+++ PortfolioDemo/src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
-  return (
+    return (
         <div className="mx-auto w-full max-w-7xl">
             <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
                 <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
@@ -11,7 +11,6 @@ const Home = () => {
                             Download Now
                             <span className="hidden sm:block text-4xl">Lorem Ipsum</span>
                         </h2>
-
                         <Link
                             className="inline-flex text-white items-center px-6 py-3 font-medium bg-orange-700 rounded-lg hover:opacity-75"
                             to="/"
@@ -30,7 +29,6 @@ const Home = () => {
                         </Link>
                     </div>
                 </div>
-
                 <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
                     <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
                 </div>
@@ -42,7 +40,7 @@ const Home = () => {
 
             <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
         </div>
-  )
+    )
 }
 
 export default Home
