577704c3ce4b7961e2adf46fc840f4ddfac7193b
SlytherinKid <mr.shubhamkr2000@gmail.com>
2025-04-17 22:10:09 +0530

    added user section on PortfolioDemo

--- PortfolioDemo/src/components/Github/Github.jsx
+++ PortfolioDemo/src/components/Github/Github.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 const Github = () => {
+    // const [data, setData] = useState([])
+    // const [instaData, setInstaData] = useState([])
+
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/hiteshchoudhary')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         setData(data)
+    //     })
+
+    // })
+
+    const data = useLoaderData()
   return (
-    <div>Github</div>
+    <div className='text-center m-4 flex align-center justify-between text-3xl'>Github followers: {data.followers}
+    <img src={data.avatar_url} alt="Git Pro Pic" />
+    </div>
   )
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return response.json()
+}
