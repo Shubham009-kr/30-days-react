6f482c45bb0c3f534007bbc2677170b8c9cf9b23
SlytherinKid <mr.shubhamkr2000@gmail.com>
2025-04-17 16:09:20 +0530

    added contact and user pages on PortfolioDemo

--- PortfolioDemo/src/components/About/About.jsx
+++ PortfolioDemo/src/components/About/About.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+      <div className="py-16 bg-white">
+            <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+                <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                    <div className="md:5/12 lg:w-5/12">
+                        <img
+                            className='w-96 ml-10 rounded-xl'
+                            src="https://instagram.fixr3-2.fna.fbcdn.net/v/t39.30808-6/470189388_18380455807110052_7358072142085928232_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0uaW1hZ2VfdXJsZ2VuLjE0NDB4MTc5OS5zZHIuZjMwODA4LmRlZmF1bHRfaW1hZ2UifQ&_nc_ht=instagram.fixr3-2.fna.fbcdn.net&_nc_cat=104&_nc_oc=Q6cZ2QF4q2X4ToTdLkh56QThTXnR0AV971N9uMEl5W2JE-Fvt-ANlBeIZQ8Pg0RXIFSm8pEGkhqBnhf-Xv93v7ebH8ri&_nc_ohc=2B4VphV0nvEQ7kNvwG2Jigp&_nc_gid=hUJBmltw4o791A29rrlC6g&edm=AP4sbd4AAAAA&ccb=7-5&ig_cache_key=MzI2NDExNTQ2OTk4MzA1MzM0Nw%3D%3D.3-ccb7-5&oh=00_AfFLKy2MEACM7hQcyZgHrJ_eQs_TSyyQ7jgxvCQuOcKVog&oe=6806BB31&_nc_sid=7a9f4b"
+                            alt="image1"
+                        />
+                    </div>
+                    <div className="md:7/12 lg:w-6/12">
+                        <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
+                            React development is carried out by passionate developers
+                        </h2>
+                        <p className="mt-6 text-gray-600">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
+                            accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
+                            aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
+                        </p>
+                        <p className="mt-4 text-gray-600">
+                            Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
+                            Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
   )
 }
 
--- PortfolioDemo/src/components/Home/Home.jsx
+++ PortfolioDemo/src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <div className="mx-auto w-full max-w-7xl">
+        <div className="mx-auto flex flex-col align-center justify-between w-full max-w-7xl">
             <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
                 <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
                     <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
@@ -29,13 +29,14 @@ const Home = () => {
                         </Link>
                     </div>
                 </div>
-                <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                    <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
+                <div className="absolute inset-0 w-full rounded-xl sm:my-20 sm:pt-1 pt-12 h-full ">
+                    <img className="w-96 rounded-xl mask-t-from-45%" src="https://instagram.fixr3-2.fna.fbcdn.net/v/t39.30808-6/470219207_18380455741110052_4501889507584950888_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0uaW1hZ2VfdXJsZ2VuLjE0NDB4MTgwMC5zZHIuZjMwODA4LmRlZmF1bHRfaW1hZ2UifQ&_nc_ht=instagram.fixr3-2.fna.fbcdn.net&_nc_cat=104&_nc_oc=Q6cZ2QE3sxyHotZTUF8293MAxmI2ypBSRDfNrqwHSJmxbVrh0Ceg36q5_enQoIFFerrbSDyoVqgEBHYdYJHTd35ViAYw&_nc_ohc=0jIhMN0-76cQ7kNvwHaSi-7&_nc_gid=u1hWKLpupLTYxvGKdsH1Gw&edm=AP4sbd4AAAAA&ccb=7-5&ig_cache_key=MzI2NDExNTQ2OTk5MTQ1MTcxMA%3D%3D.3-ccb7-5&oh=00_AfFrjIYP4O3iIoTS2I2yV1g2qWowObGFRmo4V_sXW_rDLQ&oe=68069683&_nc_sid=7a9f4b" alt="image1" />
                 </div>
             </aside>
 
-            <div className="grid  place-items-center sm:mt-20">
-                <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
+            <div className="flex align-center justify-between gap-10 px-16 place-items-center sm:mt-20">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit assumenda dolores maxime incidunt rem voluptates dignissimos aliquid amet libero. Animi fugit aut reprehenderit aliquam voluptatum sint impedit possimus voluptate deserunt, obcaecati quidem quos fuga rem qui quod iure quasi facilis dicta sit voluptates cupiditate! Suscipit autem molestiae quis ea corporis!</p>
+                <img className="rounded-xl mask-b-from-75% sm:w-96 w-48" src="https://instagram.fixr3-2.fna.fbcdn.net/v/t39.30808-6/470207359_18380455729110052_4924487063217313309_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0uaW1hZ2VfdXJsZ2VuLjE0NDB4MTgwMC5zZHIuZjMwODA4LmRlZmF1bHRfaW1hZ2UifQ&_nc_ht=instagram.fixr3-2.fna.fbcdn.net&_nc_cat=104&_nc_oc=Q6cZ2QE3sxyHotZTUF8293MAxmI2ypBSRDfNrqwHSJmxbVrh0Ceg36q5_enQoIFFerrbSDyoVqgEBHYdYJHTd35ViAYw&_nc_ohc=AH9wS9RagusQ7kNvwHiV8qx&_nc_gid=u1hWKLpupLTYxvGKdsH1Gw&edm=AP4sbd4AAAAA&ccb=7-5&ig_cache_key=MzI2NDExNTQ2OTk5MTQ5ODIyNw%3D%3D.3-ccb7-5&oh=00_AfGcx9S7QQbnJok-h1BdDGhnI8dTosPC_DR10G4veKJc1Q&oe=68069831&_nc_sid=7a9f4b" alt="image2" />
             </div>
 
             <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
