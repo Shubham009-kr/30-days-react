b0abc1ffbad3ec52bf22340923ae18f59da44c06
SlytherinKid <mr.shubhamkr2000@gmail.com>
2025-04-19 23:58:02 +0530

    added ProfileViewer

--- ThemeToggler/theme-toggler/src/App.jsx
+++ ThemeToggler/theme-toggler/src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import UserContextProvider from './context/UserContextProvider'
-import Login from './components/Login'
-import Profile from './components/Profile'
+import { ThemeProvider } from './context/theme'
 
 const App = () => {
   return (
-    <UserContextProvider>
-      {/* <Login />
-      <Profile /> */}
-    </UserContextProvider>
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+  <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       
+                    </div>
+                </div>
+  </div>
+    </ThemeProvider>
   )
 }
 
--- ThemeToggler/theme-toggler/src/main.jsx
+++ ThemeToggler/theme-toggler/src/main.jsx
@@ -18,6 +18,6 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <App />
   </StrictMode>,
 )
