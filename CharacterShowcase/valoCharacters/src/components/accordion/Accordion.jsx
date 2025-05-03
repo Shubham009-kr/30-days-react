1a5846a9be21c385bc0c889d9751e3b19e764b35
SlytherinKid <mr.shubhamkr2000@gmail.com>
2025-05-04 00:54:29 +0530

    upadted CharacterShowcase

--- CharacterShowcase/valoCharacters/src/components/accordion/Accordion.jsx
+++ CharacterShowcase/valoCharacters/src/components/accordion/Accordion.jsx
@@ -16,16 +16,17 @@ const Accordion = () => {
             {
                 data && data.length > 0 ? 
                 data.map(dataItem => <div 
-                    // style={{backgroundImage: `url(${dataItem.image})`}}
-                    className={`w-84 h-112 border-2 flex flex-col items-center rounded-[5px] bg-[url('${dataItem.image}')] bg-cover bg-center bg-clip-padding shadow-xl backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100
-`}
+                    style={{ backgroundImage: `url(${dataItem.image})` }}
+                    className={`w-84 h-112 border-2 flex flex-col items-center rounded-[5px] bg-[url(${dataItem.image})] bg-cover bg-top bg-clip-padding shadow-xl backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100
+                `}
                 >
+                    {/* <img src={dataItem.image} alt="" className='w-20 h-20' /> */}
                     <div  onClick={() => handleSingleSelection(dataItem.id)}
-                    className='flex items-center gap-8 cursor-grab h-12 w-full px-4 justify-between font-[valorant] text-[2vw] uppercase z-10'>
+                    className='flex items-center gap-8 cursor-grab h-12 w-full px-4 justify-between font-[valorant] text-[2vw] uppercase z-10 bg-gray-300/30 backdrop-filter backdrop-blur-sm'>
                         <h3 className=''>{dataItem.name}</h3>
                         <span>+</span>
                     </div>
-                    { selected === dataItem.id ? <div className='px-5 mt-45 z-10 h-full w-full'>
+                    { selected === dataItem.id ? <div className='px-5 mt-45 z-10 h-full w-full bg-gray-300/30 backdrop-filter backdrop-blur-sm'>
                         <h4 className='uppercase font-[title] italic text-[24px]'>{dataItem.role}</h4>
                         <h5 className='text-[14px] italic font-medium'>{dataItem.abilities}</h5>
                         <p className='text-[14px] py-1'>{dataItem.description}</p>
