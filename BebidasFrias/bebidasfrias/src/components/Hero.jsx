d3d362b95f5f2624eb83815f37116478598c5f87
SlytherinKid <mr.shubhamkr2000@gmail.com>
2025-07-03 00:43:57 +0530

    updated about and art section of BebidasFrias

--- BebidasFrias/bebidasfrias/src/components/Hero.jsx
+++ BebidasFrias/bebidasfrias/src/components/Hero.jsx
@@ -40,7 +40,7 @@ const Hero = () => {
             scrub:true,
             }
         }).to('.right-leaf',{y:200}, 0)
-        .to('.lefht-leaf',{y:-200}, 0)
+        .to('.left-leaf',{y:-200}, 0)
 
         const startValue = isMobile ? 'top 50%' : 'center 60%';
         const endValue = isMobile ? '120% top' : 'bottom top';
