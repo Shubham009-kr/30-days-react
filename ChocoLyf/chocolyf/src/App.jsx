287c4eca00fdb24c317f656f660f3091c3a38207
SlytherinKid <mr.shubhamkr2000@gmail.com>
2026-02-03 23:21:41 +0530

    added Footer section

--- ChocoLyf/chocolyf/src/App.jsx
+++ ChocoLyf/chocolyf/src/App.jsx
@@ -4,6 +4,8 @@ import Header from './components/Header'
 import Hero from './sections/Hero'
 import Taste from './sections/Taste'
 import Quality from './sections/Quality'
+import Parallax from './sections/Parallax'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
       <Hero />
       <Taste />
       <Quality />
+      <Parallax />
+      <Footer />
     </SmoothScroll>
   )
 }
