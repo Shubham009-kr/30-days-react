5fd8bc89431b7ee1e9c6e5f145d407a7d7012287
SlytherinKid <mr.shubhamkr2000@gmail.com>
2026-02-03 23:07:14 +0530

    updated features section

--- ChocoLyf/chocolyf/src/App.jsx
+++ ChocoLyf/chocolyf/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import SmoothScroll from './SmoothScroll'
 import Header from './components/Header'
 import Hero from './sections/Hero'
+import Taste from './sections/Taste'
 
 const App = () => {
   return (
     <SmoothScroll>
       <Header />
       <Hero />
+      <Taste />
     </SmoothScroll>
   )
 }
