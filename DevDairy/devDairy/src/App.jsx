a9c7d7fefed2eb7fca16c524acf633cb56f0338e
SlytherinKid <mr.shubhamkr2000@gmail.com>
2026-02-07 22:16:58 +0530

    added form and display functionalities in the DevDairy project

--- DevDairy/devDairy/src/App.jsx
+++ DevDairy/devDairy/src/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
+import { X } from 'lucide-react'
 
 const App = () => {
 
+  const [title, setTitle] = useState("")
+  const [code, setCode] = useState("")
+  const [comment, setComment] = useState("")
+  const [codeBlock, setCodeBlock] = useState([])
+
   const handleSubmit = (e) =>{
     e.preventDefault();
+
+    const copyCodeBlock = [...codeBlock]
+    copyCodeBlock.push({
+      title, code, comment
+    })
+
+    setCodeBlock(copyCodeBlock)
+    console.log(codeBlock)
+
+    setTitle("")
+    setCode("")
+    setComment("")
+  }
+
+  const deleteCodeBlock = (index) => {
+    const copyCodeBlock = [...codeBlock]
+    copyCodeBlock.splice(index, 1)
+    setCodeBlock(copyCodeBlock)
   }
 
   return (
@@ -12,24 +37,42 @@ const App = () => {
       onSubmit={(e)=>{
         handleSubmit(e)
       }}>
-        <input type="text" placeholder='Enter Title' className='border-1 border-gray-700 rounded-sm px-4 py-2 w-full' />
-        <textarea placeholder='Enter or Paste your code snippet here...' name="" id="" className='border-1 border-gray-700 rounded-sm px-4 py-2 w-full'></textarea>
-        <textarea placeholder='Add comment or description.' name="" id="" className='border-1 border-gray-700 rounded-sm px-4 py-2 w-full'></textarea>
-        <button className='bg-green-500 text-white px-4 py-2 w-full rounded-sm mt-4'>Save Snippet</button>
+        <input type="text" placeholder='Enter Title' className='border-1 border-gray-700 rounded-sm px-4 py-2 w-full'
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value)
+        }}
+        />
+        <textarea placeholder='Enter or Paste your code snippet here...' name="" id="" className='border-1 border-gray-700 rounded-sm px-4 py-2 w-full'
+        value={code}
+        onChange={(e) => {
+          setCode(e.target.value)
+        }}
+        ></textarea>
+        <textarea placeholder='Add comment or description.' name="" id="" className='border-1 border-gray-700 rounded-sm px-4 py-2 w-full'
+        value={comment}
+        onChange={(e) => {
+          setComment(e.target.value)
+        }}
+        ></textarea>
+        <button className='bg-green-500 active:bg-green-700 active:text-[#242424] text-white px-4 py-2 w-full rounded-sm mt-4'>Save Snippet</button>
       </form>
       <div className='lg:w-2/3 flex flex-col items-center'>
         <h1 className='text-2xl font-medium'>Your Saved Snippets</h1>
         <div className='w-full h-full mt-8 flex flex-col gap-4 overflow-auto'>
-          <div className='w-full bg-[#343434] rounded-2xl px-6 py-8 flex flex-col gap-4'>
-            <h1 className='text-xl'>Title</h1>
-            <p className='p-8 bg-[#242424] rounded-xl inset-shadow-sm inset-shadow-indigo-500/20'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi obcaecati quas non in harum fugit reprehenderit corrupti laborum odio nemo.</p>
-            <p>#Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, error.</p>
-          </div>
-          <div className='w-full bg-[#343434] rounded-2xl px-6 py-8 flex flex-col gap-4'>
-            <h1 className='text-xl'>Title</h1>
-            <p className='p-8 bg-[#242424] rounded-xl inset-shadow-sm inset-shadow-indigo-500/20'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi obcaecati quas non in harum fugit reprehenderit corrupti laborum odio nemo.</p>
-            <p>#Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, error.</p>
-          </div>
+          {codeBlock.map((item, index) => {
+            return (
+              <div key={index} className='relative w-full bg-[#343434] rounded-2xl px-6 py-8 flex flex-col gap-4'>
+                <h1 className='text-xl'>{item.title}</h1>
+                <p className='p-8 bg-[#242424] rounded-xl inset-shadow-sm inset-shadow-indigo-500/20 whitespace-pre-wrap'>{item.code}</p>
+                <p className='text-gray-400 text-sm whitespace-pre-wrap'>#{item.comment}</p>
+                <button className='absolute bottom-4 right-7 bg-red-500 active:bg-red-700 active:text-[#242424] text-white px-4 py-2 rounded-sm'
+                onClick={() => {deleteCodeBlock(index)
+                }}
+                >Delete</button>
+              </div>
+            )
+          })}
         </div>
       </div>
     </div>
